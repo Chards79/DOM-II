9838e9e2ff10ec282c517f4a6312ba0955efa62a
Eric Richards <esholundr@yahoo.com>
2019-09-03 16:51:08 -0600

    Started adding events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+// 1.
+const purpleLinks = document.querySelectorAll(".nav-link").forEach(item => {
+	item.addEventListener("mouseover", () => {
+		console.log(`click event for nav`);
+		item.style.color = "purple";
+	});
+});
+
+// 2.
+const bigTitle = document.querySelector("h1");
+bigTitle.addEventListener("mouseenter", () => {
+	console.log(`h1 click event`);
+	bigTitle.style.transform = "scale(6)";
+	bigTitle.style.transition = "all 0.1s";
+});
+
+// 3.
+const scaleDown = document.querySelector("h1");
+scaleDown.addEventListener("mouseleave", () => {
+	console.log(`h1 shrink`);
+	scaleDown.style.transform = "scale(0.01)";
+	scaleDown.style.transition = "all 2s";
+});
